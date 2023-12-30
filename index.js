779a3301bafd7f0f5e847b6ea14cfe7e6b41ce63
Talha Kaman <kamantalhaa2@gmail.com>
2023-12-30 17:07:30 +0300

    Battle & Transition

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-console.log(collisions);
 
 canvas.width = 1024
 canvas.height = 576
@@ -11,6 +10,11 @@ for (let i = 0; i < collisions.length; i += 70) {
     collisionsMap.push(collisions.slice(i, 70 + i))
 }
 
+
+const battleZonesMap = []
+for (let i = 0; i < battleZonesData.length; i += 70) {
+    battleZonesMap.push(battleZonesData.slice(i, 70 + i))
+}
 
 
 const boundaries = []
@@ -32,6 +36,24 @@ collisionsMap.forEach((row, i) => {
         )
     })
 })
+
+const battleZones = []
+
+battleZonesMap.forEach((row, i) => {
+    row.forEach((Symbol, j) => {
+        if (Symbol === 1025)
+        battleZones.push(
+            new boundary({
+                position: {
+                    x: j * boundary.width + offset.x,
+                    y: i * boundary.height + offset.y
+                }
+            })
+        )
+    })
+})
+
+console.log(battleZones)
 
 const image = new Image()
 image.src = './img/Top-Down_Game.png'
@@ -101,7 +123,7 @@ const keys = {
     }
 }
 
-const movables = [background, ...boundaries, foreground ]
+const movables = [background, ...boundaries, foreground, ...battleZones ]
 
 function rectangularCollision({rectangle1, rectangle2}) {
     return (
@@ -111,18 +133,79 @@ function rectangularCollision({rectangle1, rectangle2}) {
         rectangle1.position.y + rectangle1.height >= rectangle2.position.y
     )
 }
+const battle = {
+    initiated: false
+}
+
 function animate() {
-    window.requestAnimationFrame(animate)
+    const animationId = window.requestAnimationFrame(animate)
     background.draw() 
     boundaries.forEach((boundary) => {
         boundary.draw()
     })
+    
+    battleZones.forEach(battleZone => {
+        battleZone.draw()
+    })
     player.draw()
     foreground.draw()
 
-    
     let moving = true 
     player.moving = false 
+
+    if (battle.initiated) return
+    
+    // activate a battle
+    if (keys.w.pressed || keys.a.pressed || keys.s.pressed || keys.d.pressed) {
+        for (let i = 0; i < battleZones.length; i++) {
+            const battleZone = battleZones[i]
+            const overlappingArea = 
+            (Math.min(
+                player.position.x + player.width,
+                battleZone.position.x + battleZone.width
+              ) -
+                Math.max(player.position.x, battleZone.position.x)) *
+              (Math.min(
+                player.position.y + player.height,
+                battleZone.position.y + battleZone.height
+              ) -
+                Math.max(player.position.y, battleZone.position.y))
+            if (
+                rectangularCollision({
+                    rectangle1: player,
+                    rectangle2: battleZone
+                }) &&
+                overlappingArea > (player.width * player.height) / 2 &&
+                Math.random() < 0.002
+            )   {
+                console.log('activate battle')
+
+                // deactivate current animation loop
+                window.cancelAnimationFrame(animationId)
+
+                battle.initiated = true
+                gsap.to('#overlappingDiv', {
+                    opacity: 1,
+                    repeat: 3,
+                    yoyo: true,
+                    duration: 0.2,
+                    onComplete() {
+                        gsap.to('#overlappingDiv', {
+                            opacity: 1,
+                            duration: 0.2
+                        })
+
+                        // activate a new animation loop
+                        animateBattle()
+                    }
+                })
+                break 
+            }
+        }
+    }
+
+
+    
     if (keys.w.pressed && lastkey === 'w') {
         player.moving = true 
         player.image = player.Sprites.up
@@ -140,11 +223,11 @@ function animate() {
                     }
                 })
             )   {
-                console.log('colliding')
-            moving = false
+                moving = false
                 break 
             }
         }
+
 
         if (moving)
         movables.forEach((movable) => {
@@ -235,6 +318,11 @@ function animate() {
     }
 }
 animate()
+
+function animateBattle() {
+    window.requestAnimationFrame(animateBattle)
+    console.log('animating battle')
+}
 
 let lastkey = ''
 window.addEventListener('keydown', (e) => {
